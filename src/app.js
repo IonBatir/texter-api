e58d55d6f3ff33f7464bf826ae37d2c68b3a8c7f
Ion Batîr <ionbatir@gmail.com>
2021-04-27 17:47:56 +0300

    Connect to DB

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const helmet = require('helmet');
+
+const {MONGO_URI} = require('./secrets');
 const {PORT} = require('./constants');
 
-const app = express();
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    const app = express();
 
-app.use(helmet());
+    app.use(helmet());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}...`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT ${PORT}...`);
+    });
+}).catch(console.error);
+
