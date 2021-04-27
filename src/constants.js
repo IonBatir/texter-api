d36ef96b1d27d4d7daa7c6f246afe5c7e500007a
Ion Batîr <ionbatir@gmail.com>
2021-04-27 18:41:44 +0300

    Add email regex to constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 const PORT = process.env.PORT ?? 3000;
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 module.exports = {
-    PORT
+    PORT,
+    EMAIL_REGEX
 };
