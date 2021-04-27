6241461fd05824d5fd99baff41edccfd5b5c4ade
Ion Batîr <ionbatir@gmail.com>
2021-04-27 16:57:50 +0300

    Init express app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+const express = require('express');
+const {PORT} = require('./constants');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}...`);
+});
