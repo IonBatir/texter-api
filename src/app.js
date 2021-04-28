884e6ef7456933fd80ec507c1cdfaea2f06af9aa
Ion Batîr <ionbatir@gmail.com>
2021-04-28 22:06:25 +0300

    Use user router

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const helmet = require('helmet');
 
+const userRouter = require('./routes/user');
 const {MONGO_URI} = require('./secrets');
 const {PORT} = require('./constants');
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(() => {
     const app = express();
 
     app.use(helmet());
@@ -16,6 +17,8 @@ mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).t
     app.get('/', (req, res) => {
         res.send('Hello World!');
     });
+
+    app.use(userRouter);
 
     app.listen(PORT, () => {
         console.log(`Server is running on PORT ${PORT}...`);
